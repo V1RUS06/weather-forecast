37c57650ae1a56b31a87de7d521cb8a39187ed6c
Denis <fomin.da@bokus.ru>
2021-12-24 12:48:05 +0300

    запрос на получение данных

--- App.tsx
+++ App.tsx
@@ -1,7 +1,25 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import {Home} from "./src/screens/Home";
+import {useLocation} from "./src/hooks/useLocation";
+import {Text} from "react-native";
 
 export default function App() {
+  const {longitude, latitude, loading} = useLocation()
+  const [weather, setWeather] = useState()
+
+  // useEffect(() => {
+  //   (async () => {
+  //     if (longitude && latitude) {
+  //       const weather: IWeather= await getWeatherApi(longitude, latitude)
+  //
+  //       setWeather(weather)
+  //     }
+  //   })()
+  // }, []);
+
+  // if (loading || !weather) {
+  //   return <Text style={{alignItems: 'center', justifyContent: 'center', flex: 1}}>Загрузка данных...</Text>
+  // }
   return (
 
       <Home />
